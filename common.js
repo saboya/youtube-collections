b46e37b6df1e237944298c77e4fcacb762dac167
Rodrigo Saboya <saboya@users.noreply.github.com>
2016-08-27 23:21:10 -0300

    Moving a lot of code from guide.js to common.js, since we will always have the guide available for information.

--- common.js
+++ common.js
@@ -6,3 +6,64 @@ const __collections = storage.get('collections').then(item => {
 const __subscriptions = storage.get('subscriptions').then(item => {
   return item.subscriptions
 })
+
+function _getGuideSection() {
+  return document.querySelector('#guide-subscriptions-section')
+}
+
+function _getSubscriptionsSection () {
+  var _guide_element = document.querySelector('#guide')
+  return new Promise((resolve, reject) => {
+    if (_getGuideSection() !== null) {
+      resolve(_getGuideSection())
+    } else {
+      var observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          if (mutation.type === 'childList' && mutation.target === _guide_element) {
+            observer.disconnect()
+            observer.takeRecords()
+            if (_getGuideSection() !== null) {
+              resolve(_getGuideSection())
+            } else {
+              reject('Subscriptions not found in Guide')
+            }
+          }
+        })
+      })
+
+      observer.observe(_guide_element, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        characterData: true
+      })
+    }
+  })
+}
+
+Promise.all([
+  _getSubscriptionsSection(),
+  __collections,
+  __subscriptions
+]).then(valueArr => {
+  var subSection = valueArr[0]
+  var collections = valueArr[1]
+  var subscriptions = valueArr[2]
+
+  subSection.querySelectorAll('#guide-channels > li.guide-channel').forEach(node => {
+    let channelId = node.querySelector('a').getAttribute('data-external-id')
+    subscriptions[channelId] = {
+      id: channelId,
+      count: node.querySelector('span.no-count') ? 0 : parseInt(node.querySelector('.guide-count-value').textContent),
+      name: node.querySelector('a.guide-item').getAttribute('title'),
+      collection: subscriptions[channelId] !== undefined ? collections[subscriptions[channelId]] : null
+    }
+
+    if(subscriptions[channelId].collection !== null) {
+      if(subscriptions[channelId].collection.count === undefined) {
+        subscriptions[channelId].collection.count = 0
+      }
+      subscriptions[channelId].collection.count += subscriptions[channelId].count
+    }
+  })
+})
--- guide.js
+++ guide.js
@@ -1,65 +1,6 @@
 'use strict'
 
-const _guide_element = document.querySelector('#guide')
-
 // chrome.tabs.insertCSS(integer tabId, object details, function callback)
-
-function _getGuideSection() {
-  return document.querySelector('#guide-subscriptions-section')
-}
-
-function _getHtmlForGuideButton (title,count) {
-  return '<li class="guide-channel collection-item guide-notification-item overflowable-list-item" role="menuitem">\
-  <a class="guide-item yt-uix-sessionlink yt-valign spf-link" href="#" title="'+title+'">\
-    <span class="yt-valign-container">\
-      <span class="thumb">\
-        <span class="video-thumb  yt-thumb yt-thumb-20">\
-          <span class="yt-thumb-square">\
-            <span class="yt-thumb-clip">\
-                <span class="vertical-align"></span>\
-            </span>\
-          </span>\
-        </span>\
-      </span>\
-      <span class="display-name ">\
-        <span>'+title+'</span>\
-      </span>\
-    </span>\
-    <span class="guide-count yt-uix-tooltip yt-valign">\
-      <span class="yt-valign-container guide-count-value">'+count+'</span>\
-    </span>\
-  </a>\
-</li>'
-}
-
-function _getSubscriptionsSection () {
-  return new Promise((resolve, reject) => {
-    if (_getGuideSection() !== null) {
-      resolve(_getGuideSection())
-    } else {
-      var observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-          if (mutation.type === 'childList' && mutation.target === _guide_element) {
-            observer.disconnect()
-            observer.takeRecords()
-            if (_getGuideSection() !== null) {
-              resolve(_getGuideSection())
-            }else {
-              reject('Subscriptions not found in Guide')
-            }
-          }
-        })
-      })
-
-      observer.observe(_guide_element, {
-        childList: true,
-        subtree: true,
-        attributes: true,
-        characterData: true
-      })
-    }
-  })
-}
 
 function _initTestDb () {
   var goodId = HashID.generate()
@@ -98,20 +39,6 @@ Promise.all([
   var subSection = valueArr[0]
   var collections = valueArr[1]
   var subscriptions = valueArr[2]
-  var channels = []
-
-  subSection.querySelectorAll('#guide-channels > li.guide-channel').forEach(node => {
-    let channelId = node.querySelector('a').getAttribute('data-external-id')
-      if(subscriptions[channelId] !== undefined) {
-      channels.push({
-        id: channelId,
-        count: node.querySelector('span.no-count') ? 0 : parseInt(node.querySelector('.guide-count-value').textContent),
-        name: node.querySelector('a.guide-item').getAttribute('title'),
-        node: node,
-        collection: subscriptions[channelId]
-      })
-    }
-  })
 
   template.render('guide-section',{title:'Collections'}).then(html => {
     var node = document.createElement('li')
@@ -122,7 +49,7 @@ Promise.all([
     return Object.keys(collections).map(id => {
       return template.render('guide-section-item',{
         name: collections[id].name,
-        count: channels.filter(channel => channel.collection === id).reduce((a,c) => a + c.count,0)
+        count: collections[id].count
       }).then(html => {
         var elem = document.createElement('li')
         node.appendChild(elem)
