bd3957c4a813f0b5678849d68d04207f69b47e6c
Rodrigo Saboya <saboya@users.noreply.github.com>
2016-08-28 02:17:09 -0300

    Adding management section for collections. Adding and removing a collection works.

--- manager.js
+++ manager.js
@@ -4,3 +4,63 @@ document.querySelectorAll('#subscription-manager-list tr.subscription-item').for
   console.log(element.getAttribute('data-subscription-id'))
 })
 
+function _getSubscriptionContainer() {
+  return document.getElementById('subscription-manager-container')
+}
+
+Promise.all([
+  __collections,
+  __subscriptions
+]).then(valueArr => {
+  var collections = valueArr[0]
+  var subscriptions = valueArr[1]
+
+  template.render('manager-section',{ quantity: Object.keys(collections).length }).then(html => {
+    var elem = document.createElement('div')
+    _getSubscriptionContainer().parentNode.insertBefore(elem,_getSubscriptionContainer())
+    elem.outerHTML = html
+
+    return document.getElementById('collections-manager-container')
+  }).then(node => {
+    var form = document.getElementById('add-collection-form')
+
+    node.addEventListener('click',e => {
+      if(e.target.hasAttribute('data-collection-id')) {
+        var removedId = e.target.dataset.collectionId
+        if(window.confirm('Remove collection '+ collections[removedId].name+'?')) {
+          _removeCollection(e.target.dataset.collectionId)
+        }
+      }
+    })
+
+    form.querySelector('input[name="add-collection"]').addEventListener('input',e => {
+      e.target.classList.toggle('invalid',!e.target.checkValidity() && e.target.value !== '')
+    })
+
+    form.querySelector('input[name="add-collection"]').addEventListener('invalid',e => {
+      e.target.classList.add('invalid')
+    })
+
+    form.addEventListener('submit',e => {
+      _addCollection(form.querySelector('input[name="add-collection"]').value)
+
+      e.preventDefault()
+      e.stopPropagation()
+      return false
+    })
+
+    return document.querySelector('#collection-manager-list tbody')
+  }).then(node => {
+    return Object.keys(collections).map(id => {
+      return template.render('manager-section-item',{
+        id: id,
+        name: collections[id].name
+      }).then(html => {
+        var elem = document.createElement('tr')
+        node.appendChild(elem)
+        elem.outerHTML = html
+        return document.getElementById(id+'-manager-collection')
+      })
+    })
+  })
+})
