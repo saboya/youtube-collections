a62d2138a20a2b51a6bf246cd0a70da578334ff3
Rodrigo Saboya <saboya@users.noreply.github.com>
2016-08-28 17:44:53 -0300

    Adding collections subsciptions to the guide.

--- guide.js
+++ guide.js
@@ -15,14 +15,38 @@ Promise.all([
     node.outerHTML = html
     return document.querySelector('#guide-collection-list .guide-channels-list')
   }).then(node => {
+    node.addEventListener('click',e => {
+      if(e.target.closest('.collection-item a').hasAttribute('data-collection-id')) {
+        var height = 28
+        if(e.target.closest('.collection-item').classList.toggle('collection-list-open')) {
+          var id = e.target.closest('.collection-item a').dataset.collectionId
+          var listNode = document.getElementById(id+'-guide-channel-list')
+          height += listNode.offsetHeight
+        }
+        e.target.closest('.collection-item').style.maxHeight = height+'px'
+      }
+    })
+
+    return document.querySelector('#guide-collection-list .guide-channels-list')
+  }).then(node => {
     return Object.keys(collections).map(id => {
       return template.render('guide-section-item',{
+        id: id,
         name: collections[id].name,
         count: collections[id].count || ''
       }).then(html => {
         var elem = document.createElement('li')
         node.appendChild(elem)
         elem.outerHTML = html
+
+        Object.keys(subscriptions).filter(key => subscriptions[key].collectionId === id).forEach(key => {
+          var newNode = document.getElementById(key+'-guide-item').cloneNode(true)
+          newNode.removeAttribute('id')
+          var thumb = newNode.querySelector('img')
+          thumb.setAttribute('src',thumb.dataset.thumb)
+
+          document.getElementById(id+'-guide-channel-list').appendChild(newNode)
+        })
       })
     })
   })
